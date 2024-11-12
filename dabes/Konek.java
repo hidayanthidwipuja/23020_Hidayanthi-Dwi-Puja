f835ff6dc50b66c4a2af3144bead63a450a677ec
hidayanthidwipuja <147781209+hidayanthidwipuja@users.noreply.github.com>
2024-11-12 20:19:46 +0700

    pertemuan11

---
+++ dabes/Konek.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dabes;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author UseR
+ */
+public class Konek{
+      private static Connection konek;
+
+    public static Connection configDB() throws SQLException, ClassNotFoundException {
+        try {
+            if (konek == null) {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                konek = DriverManager.getConnection("jdbc:mysql://localhost:3306/login", 
+                        "root", "");
+                System.out.println("Koneksi berhasil!");
+            }
+            return konek;
+        } catch (SQLException e) {
+            System.err.println("Error: Koneksi tidak berhasil! " + e.getMessage());
+            throw e;
+        }
+    }
+}
